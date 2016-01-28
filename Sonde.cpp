f3769ff864b6327c731048fdc0932784b9e45fa3
Tolcantor <guiblinmaxime@gmail.com>
2016-01-28 04:01:57 +0100

    Create Sonde.cpp

---
+++ Sonde.cpp
@@ -0,0 +1,20 @@
+#include "Sonde.h"
+#include "Astre.h"
+
+Sonde::Sonde(Astre *a, int nbA)
+{
+    //ctor
+}
+
+Sonde::~Sonde()
+{
+    //dtor
+}
+
+Sonde::EDP(double t, double* X, double* dx){
+
+}
+
+Sonde::Euler(double *X, double t, double dt ){
+
+}
